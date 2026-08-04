97b02d0002caf44a6a18587ed1abdc11cccabd59
mei30 <meisamjafarimosleh@gmail.com>
2026-08-04 17:23:40 +0330

    Add simple level-based architecture support

---
+++ Assets/Levels/Scripts/SaveManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
